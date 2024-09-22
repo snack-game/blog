030dd9c22ea54efb1d96ab253b8f6d76cb1019ef
0chil <gorae02@gmail.com>
2024-09-22 07:39:34 +0000

    feat(nagivation): intersection 기반 prefetch를 사용한다

--- cloudflare-worker/static/onNavigateCompleted.js
+++ cloudflare-worker/static/onNavigateCompleted.js
@@ -1,25 +1,42 @@
-function onNagivateCompleted() {
-    document.dispatchEvent(new Event('onNagivateCompleted'));
-    const links = document.querySelectorAll("a.notion-link");
-    links.forEach(link => {
-        const href = link.getAttribute("href");
-        if (href && href.startsWith("/")) {
+// Observer for prefetch
+const callback = (entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            const href = entry.target.getAttribute("href");
             window.next.router.prefetch(href);
+            observer.unobserve(entry.target);
         }
     });
+};
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.1
+};
+const observer = new IntersectionObserver(callback, options);
+
+function onNagivateCompleted() {
+    document.dispatchEvent(new Event('onNagivateCompleted'));
+
+    const links = document.querySelectorAll('a.notion-link[href]');
+    Array.from(links)
+        .filter(link => link.getAttribute("href").startsWith("/"))
+        .forEach(link => observer.observe(link));
 }
+document.addEventListener("DOMContentLoaded", () => setTimeout(onNagivateCompleted, 500));
 
 // Hook Navigates
 const originalPushState = history.pushState;
 const originalReplaceState = history.replaceState;
 history.pushState = function () {
+    observer.disconnect();
     const result = originalPushState.apply(history, arguments);
     setTimeout(onNagivateCompleted, 500);
     return result;
 };
 history.replaceState = function () {
+    observer.disconnect();
     const result = originalReplaceState.apply(history, arguments);
     setTimeout(onNagivateCompleted, 500);
     return result;
 };
-document.addEventListener("DOMContentLoaded", () => setTimeout(onNagivateCompleted, 500));
